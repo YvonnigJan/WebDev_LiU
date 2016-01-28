f1846e6fc644594a473eeb4267a29dc85dc6f8e2
Cindy Najjar <cindy06000@gmail.com>
2016-01-28 08:42:28 +0100

    ajout profile view + modif displayMsg dans .js

--- client.js
+++ client.js
@@ -5,7 +5,7 @@
 var sizeMinPwd = 6;
 
 displayView = function() {
-		document.getElementById("welcome").innerHTML = document.getElementById("welcomeview").innerHTML;
+		document.getElementById("page").innerHTML = document.getElementById("welcomeview").innerHTML;
 };
 
 window.onload = function() {
@@ -27,35 +27,35 @@ signUp = function() {
 
 checkPwds = function(pw1, pw2) {
 	if (pw1.value != pw2.value) {
-		displayMsg("Error: both passwords must be identical","1");
+		displayMsg("Error: both passwords must be identical","error");
 	} else {
-		displayMsg("","0");
+		displayMsg("","success");
 	}
 };
 
 lengthPwd = function(pw) {
 	if (pw.value.length < sizeMinPwd) {
-		displayMsg("Password must be at least 6 characters long !","1");
+		displayMsg("Password must be at least 6 characters long !","error");
 	} else {
-		displayMsg("","0");
+		displayMsg("","success");
 	}
 
 }
 
 /* message : the message to be shown
-state : 0 -> no error, 1 -> error */
+state : error or success */
 displayMsg = function(message,state) {
 
 	var errFrame = document.getElementById("displayMsg");
 
 	errFrame.innerHTML = message;
 	
-	if (state == "1") {
+	if (state == "error") {
 		errFrame.style.border = "1px solid red";
 		errFrame.style.backgroundColor = "white";
 	}
 		
-	else if (state == "0") {
+	else if (state == "success") {
 		errFrame.style.border = "";
 		errFrame.style.backgroundColor = "";
 	}
